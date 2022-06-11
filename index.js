60b24845d7a6cd7cd2d9e7fe43ba3510d00d2e18
Prakriti Pokharel <101075967+iprakritip@users.noreply.github.com>
2022-06-11 10:31:11 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+document.getElementById("submitBtn").addEventListener("click", function(e){
+  var yearZie = document.getElementById("year").value;
+
+  if(yearZie%4==0 && yearZie%100!==0 || yearZie%400==0){
+  //  console.log("issa leap year");
+    document.getElementById("message").innerHTML = yearZie + " is a leap year.";
+    document.getElementById("message").classList.add("leap");
+  } else{
+  //console.log("not a leap year");
+  document.getElementById("message").innerHTML =yearZie + " is not a leap year.";
+  document.getElementById("message").classList.add("notLeap");
+  };
+});
